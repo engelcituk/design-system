745716c328a2ad9c763930c0a8aa51fd663235bc
cituk <ecituk@thefives.com.mx>
2023-02-10 02:12:48 -0500

    avances docs css

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,70 +1,75 @@
 import { defineConfig } from 'vitepress';
+import { withMermaid } from "vitepress-plugin-mermaid";
 
 // refer https://vitepress.vuejs.org/config/introduction for details
-export default defineConfig({
+const config = {
   lang: 'es_MX',
-  title: 'Sistema de diseño',
-  description: 'Vite & Vue powered static site generator.',
+    title: 'Sistema de diseño',
+    description: 'Vite & Vue powered static site generator.',
+  
+    themeConfig: {
+      nav: [
+      //   { text: 'Example', link: '/example' },
+  
+        // {
+        //   text: 'Dropdown Menu',
+        //   items: [
+        //     { text: 'Item A', link: '/item-1' },
+        //     { text: 'Item B', link: '/item-2' },
+        //     { text: 'Item C', link: '/item-3' },
+        //   ],
+        // },
+  
+        // ...
+      ],
+  
+      sidebar: [
+        {
+          text: 'Bases',
+          collapsed: false,
+          items: [
+            { text: 'Colores', link: '/guide/colors' },    
+            { text: 'Tipografía', link: '/guide/typography' },                    
+            { text: 'Íconos', link: '/guide/icons' },                    
+  
+          ],
+        },
+        {
+          text: 'CSS Básico Intro',
+          collapsed: true,
+          items: [
+            { text: 'Selectores', link: '/guide/css/basic/selectors' },    
+            { text: 'Pseudo-clases', link: '/guide/typography' },                    
+            { text: 'Pseudo-elementos', link: '/guide/typography' },                    
+            { text: 'Especificidad', link: '/guide/typography' },                    
+            { text: 'Valores y unidades', link: '/guide/typography' },                    
+            { text: 'Propiedades y texto', link: '/guide/typography' },                    
+            { text: 'Box Model',
+              items:[
+                { text: 'Margin', link: '/guide/typography' },                    
+                { text: 'Padding', link: '/guide/typography' },                    
+                { text: 'Box-sizing', link: '/guide/typography' },                    
+                { text: 'Border Radius', link: '/guide/typography' },                    
+              ]
+            },
+            { text: 'Outline', link: '/guide/typography' },                    
+            { text: 'Background', link: '/guide/typography' },                    
+            { text: 'Gradientes', link: '/guide/typography' },                    
+            { text: 'Box shadow', link: '/guide/typography' },                    
+            { text: 'Variables', link: '/guide/typography' },                    
+            { text: 'Posicionamiento', link: '/guide/typography' },                    
+            { text: 'Overflow', link: '/guide/typography' },                    
+            { text: 'Display', link: '/guide/typography' },                    
+            { text: 'Flexbox', link: '/guide/typography' },                    
+            { text: 'Grid', link: '/guide/typography' },                    
+            { text: 'Animaciones', link: '/guide/typography' },                    
+  
+          ],
+        },
+      ],
+    },
+}
 
-  themeConfig: {
-    nav: [
-    //   { text: 'Example', link: '/example' },
 
-      // {
-      //   text: 'Dropdown Menu',
-      //   items: [
-      //     { text: 'Item A', link: '/item-1' },
-      //     { text: 'Item B', link: '/item-2' },
-      //     { text: 'Item C', link: '/item-3' },
-      //   ],
-      // },
-
-      // ...
-    ],
-
-    sidebar: [
-      {
-        text: 'Bases',
-        collapsed: false,
-        items: [
-          { text: 'Colores', link: '/guide/colors' },    
-          { text: 'Tipografía', link: '/guide/typography' },                    
-          { text: 'Íconos', link: '/guide/icons' },                    
-
-        ],
-      },
-      {
-        text: 'CSS Básico Intro',
-        collapsed: true,
-        items: [
-          { text: 'Selectores', link: '/guide/css/basic/selectors' },    
-          { text: 'Pseudo-clases', link: '/guide/typography' },                    
-          { text: 'Pseudo-elementos', link: '/guide/typography' },                    
-          { text: 'Especificidad', link: '/guide/typography' },                    
-          { text: 'Valores y unidades', link: '/guide/typography' },                    
-          { text: 'Propiedades y texto', link: '/guide/typography' },                    
-          { text: 'Box Model',
-            items:[
-              { text: 'Margin', link: '/guide/typography' },                    
-              { text: 'Padding', link: '/guide/typography' },                    
-              { text: 'Box-sizing', link: '/guide/typography' },                    
-              { text: 'Border Radius', link: '/guide/typography' },                    
-            ]
-          },
-          { text: 'Outline', link: '/guide/typography' },                    
-          { text: 'Background', link: '/guide/typography' },                    
-          { text: 'Gradientes', link: '/guide/typography' },                    
-          { text: 'Box shadow', link: '/guide/typography' },                    
-          { text: 'Variables', link: '/guide/typography' },                    
-          { text: 'Posicionamiento', link: '/guide/typography' },                    
-          { text: 'Overflow', link: '/guide/typography' },                    
-          { text: 'Display', link: '/guide/typography' },                    
-          { text: 'Flexbox', link: '/guide/typography' },                    
-          { text: 'Grid', link: '/guide/typography' },                    
-          { text: 'Animaciones', link: '/guide/typography' },                    
-
-        ],
-      },
-    ],
-  },
-});
+// export default defineConfig(config)
+export default defineConfig(config)
--- docs/guide/css/basic/selectors.md
+++ docs/guide/css/basic/selectors.md
@@ -15,7 +15,7 @@ Supongamos que queremos que distintos elementos tengan un mismo estilo.
 h2 { color: gray }
 p { color: gray }
 ```
-De esta manera es más sencilla y te ahorrar líneas de CSS
+De esta manera es más sencilla y te ahorras líneas de CSS
 
 ```css
 h2, p { color: gray }
@@ -23,9 +23,103 @@ h2, p { color: gray }
 
 ## Selector universal
 
+El selector universal (*) selecciona todos los elementos del HTML
+
+```css
+*{
+    margin: 0;
+ }
+```
+Actúa como un 'comodín' que coincide con cualquier elemento.
+
 ## Selector de clase
+
+Es la forma más común de aplicar estilos. Para que los selectores de clase funcionen y asociar los estilos de esta clase a algún elemento, simplemente debemos de agregar el atributo class a ese elemento.
+
+```html
+<p class="content"> Estamos aprendiendo css</p>
+```
+
+Ahora agregamos los estilos de la clase
+
+```css
+.content{
+    color: pink;
+    font-size: 1.5rem;
+}
+```
+
+::: tip
+No olvides colocar el punto (.) antes de la clase CSS
+:::
+
+En el HTML es posible agregar más de una clase a un elemento.
+
+```html
+<p class="content contenedor">La inspiración desbloquea el futuro. </p>
+<h2 class="content contenedor">Toma riesgos y prepárate para fallar</h2>
+```
+
+Aquí se agregará la fuente bold unicamente al elemento **h2** con clase **content**, no así al **p**. :point_down:
+
+
+```css
+.content{
+    color: pink;
+    font-size: 1.5rem;
+}
+
+h2.content{
+    font-weight: bold;
+}
+```
 
 ## Selector ID
 
+De cierta forma el selector por **ID** es similar al selector de clase, excepto por:
+- Al selector ID lo precede un hash (#)
+- El id puede utilizarse en un único elemento del HTML (las clases pueden ser asignadas/reusadas a varios elementos).
+
+```html
+<p id="quote">
+    Un desarrollador de software no implementa código
+    limpio con un látigo atrás
+</p>
+```
+:point_down:
+
+```css
+#quote{
+    background-color: yellow;
+    color: #ffffff;
+}
+```
+
 ## Selectores descendientes
 
+Ahora vamos a entender como funciona la estructura de un documento HTML, para así comprender la relación de elementos **padre-hijo**:
+
+```html
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mis apuntes</title>
+</head>
+<body>
+    <h1>Apuntes<em>sobre css</em> </h1>
+    <p> Apuntes <em>disponibles</em> </p>
+    <ol>
+        <li>JavaScript</li>
+        <li>VueJs</li>
+        <li>Python</li>
+    </ol>
+    <p>
+        Recomienda temas en 
+        <a href="#">enlace a social network</a>
+    </p>
+</body>
+</html>
+```
