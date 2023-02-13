e3d37fa5e3604af5fcb5d9fd28a9d088ee2f01a8
eCituk <ecituk@thefives.com.mx>
2023-02-13 09:03:21 -0500

    title 3

--- docs/components/typography/FontStyles.vue
+++ docs/components/typography/FontStyles.vue
@@ -2,98 +2,98 @@
     <div class="container">
         <div class="typography__content">
             <div class="text">
-                <p class="title-paragraph">Title 1 | Font size: 60px | Line height: 72px | Weight: Bold/700 | Letter Spacing: -0.5% </p>
+                <p class="title-paragraph">Title 1 | Font size: 60px | Line height: 72px | Weight: Bold/700 </p>
                 <p class="title-1">
                     La Tierra es el tercer planeta del sistema solar. 
                 </p>
             </div>
 
             <div class="text">
-                <p class="title-paragraph">Title 2 | Font size: 48px | Line height: 72px | Weight: Bold/700 | Letter Spacing: -0.5% </p>
+                <p class="title-paragraph">Title 2 | Font size: 48px | Line height: 116% | Weight: Bold/700 </p>
                 <p class="title-2">
                     La Tierra es el tercer planeta del sistema solar. 
                 </p>
             </div>
 
             <div class="text">
-                <p class="title-paragraph">Title 3 | Font size: 60px | Line height: 72px | Weight: Bold/700 | Letter Spacing: -0.5% </p>
+                <p class="title-paragraph">Title 3 | Font size: 34px | Line height: 39.78px | Weight: ExtraBold/800 </p>
+                <p class="title-3">
+                    La Tierra es el tercer planeta del sistema solar. 
+                </p>
+            </div>
+
+            <div class="text">
+                <p class="title-paragraph">Title 4 | Font size: 60px | Line height: 72px | Weight: Bold/700 </p>
                 <p class="title-1">
                     La Tierra es el tercer planeta del sistema solar. 
                 </p>
             </div>
 
             <div class="text">
-                <p class="title-paragraph">Title 4 | Font size: 60px | Line height: 72px | Weight: Bold/700 | Letter Spacing: -0.5% </p>
+                <p class="title-paragraph">Headline  | Font size: 60px | Line height: 72px | Weight: Bold/700 </p>
                 <p class="title-1">
                     La Tierra es el tercer planeta del sistema solar. 
                 </p>
             </div>
 
             <div class="text">
-                <p class="title-paragraph">Headline  | Font size: 60px | Line height: 72px | Weight: Bold/700 | Letter Spacing: -0.5% </p>
+                <p class="title-paragraph">Subheadline | Font size: 60px | Line height: 72px | Weight: Bold/700 </p>
                 <p class="title-1">
                     La Tierra es el tercer planeta del sistema solar. 
                 </p>
             </div>
 
             <div class="text">
-                <p class="title-paragraph">Subheadline | Font size: 60px | Line height: 72px | Weight: Bold/700 | Letter Spacing: -0.5% </p>
+                <p class="title-paragraph">Body  | Font size: 60px | Line height: 72px | Weight: Bold/700 </p>
                 <p class="title-1">
                     La Tierra es el tercer planeta del sistema solar. 
                 </p>
             </div>
 
             <div class="text">
-                <p class="title-paragraph">Body  | Font size: 60px | Line height: 72px | Weight: Bold/700 | Letter Spacing: -0.5% </p>
+                <p class="title-paragraph">Body Bold | Font size: 60px | Line height: 72px | Weight: Bold/700 </p>
                 <p class="title-1">
                     La Tierra es el tercer planeta del sistema solar. 
                 </p>
             </div>
 
             <div class="text">
-                <p class="title-paragraph">Body Bold | Font size: 60px | Line height: 72px | Weight: Bold/700 | Letter Spacing: -0.5% </p>
+                <p class="title-paragraph">Body Small | Font size: 60px | Line height: 72px | Weight: Bold/700 </p>
                 <p class="title-1">
                     La Tierra es el tercer planeta del sistema solar. 
                 </p>
             </div>
 
             <div class="text">
-                <p class="title-paragraph">Body Small | Font size: 60px | Line height: 72px | Weight: Bold/700 | Letter Spacing: -0.5% </p>
+                <p class="title-paragraph">Body Small Bold | Font size: 60px | Line height: 72px | Weight: Bold/700 </p>
                 <p class="title-1">
                     La Tierra es el tercer planeta del sistema solar. 
                 </p>
             </div>
 
             <div class="text">
-                <p class="title-paragraph">Body Small Bold | Font size: 60px | Line height: 72px | Weight: Bold/700 | Letter Spacing: -0.5% </p>
+                <p class="title-paragraph">Caption | Font size: 60px | Line height: 72px | Weight: Bold/700 </p>
                 <p class="title-1">
                     La Tierra es el tercer planeta del sistema solar. 
                 </p>
             </div>
 
             <div class="text">
-                <p class="title-paragraph">Caption | Font size: 60px | Line height: 72px | Weight: Bold/700 | Letter Spacing: -0.5% </p>
-                <p class="title-1">
-                    La Tierra es el tercer planeta del sistema solar. 
-                </p>
-            </div>
-
-            <div class="text">
-                <p class="title-paragraph">Button large | Font size: 60px | Line height: 72px | Weight: Bold/700 | Letter Spacing: -0.5% </p>
+                <p class="title-paragraph">Button large | Font size: 60px | Line height: 72px | Weight: Bold/700 </p>
                 <p class="title-1">
                     Button Large
                 </p>
             </div>
 
             <div class="text">
-                <p class="title-paragraph">Button medium | Font size: 60px | Line height: 72px | Weight: Bold/700 | Letter Spacing: -0.5% </p>
+                <p class="title-paragraph">Button medium | Font size: 60px | Line height: 72px | Weight: Bold/700 </p>
                 <p class="title-1">
                     Button Medium
                 </p>
             </div>
 
             <div class="text">
-                <p class="title-paragraph">Button small | Font size: 60px | Line height: 72px | Weight: Bold/700 | Letter Spacing: -0.5% </p>
+                <p class="title-paragraph">Button small | Font size: 60px | Line height: 72px | Weight: Bold/700 </p>
                 <p class="button-small">
                     Button Small
                 </p>
@@ -145,7 +145,15 @@
 .title-2{
     font-weight: 700;
     font-size: 48px;
-    line-height: 56px;
+    line-height: 116%;
+    /* color: #0F0D15; */
+}
+
+.title-3{
+    font-weight: 800;
+    font-size: 34px;
+    line-height: 117%;
+    /* color: #0F0D15; */
 }
 
 .button-small{
