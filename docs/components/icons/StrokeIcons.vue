79c1afb066016740fa42fb0a343bce34cd4aebde
cituk <ecituk@thefives.com.mx>
2023-02-14 00:59:08 -0500

    se inicia con icons

---
+++ docs/components/icons/StrokeIcons.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+        <h3>Basics</h3>
+        <div class="icons">
+            <svg width="32" height="32" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" class="icon__basic">
+                <path d="M7.5 4V11M4 7.5H11" stroke="#1D1929"/>
+            </svg>    
+        </div>    
+    </div>
+</template>
+
+
+
+<style scoped>
+.container {
+    width: 100%;
+    margin: 0 auto;
+    overflow: hidden;
+    /* max-width: 900px; */
+}
+
+.icons{
+    margin-top: 1rem;
+    display: flex;
+    justify-content: space-between;
+    gap: 1rem;    
+    flex-wrap: wrap;
+}
+
+.icon__basic{
+    cursor: pointer;
+}
+</style>
--- docs/guide/icons.md
+++ docs/guide/icons.md
@@ -1,1 +1,43 @@
-lorem ccolors
+<script setup>
+    import StrokeIcons from '../components/icons/StrokeIcons.vue'
+    import FontStyles from '../components/typography/FontStyles.vue'
+</script>
+
+# Iconos
+
+
+## Stroke
+
+<StrokeIcons/>
+
+### Essentials
+### Emoji
+### Interface
+### Commerfe
+### Finance
+### Technology
+### Charts
+### Authoring
+### Real estate
+### Arrows
+### File & Folders
+### Tasks, chat & events
+### Social
+### Browsers
+### Design & Dev
+
+
+
+
+
+
+
+
+
+
+
+
+
+## Fill 
+
+## Duotone
